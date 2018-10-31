4849d3b38697821f049ebfb44a6795dd358cf8f0
ramyakrishnan86 <ramya_krishnan86@yahoo.co.in>
2018-10-31 16:07:58 +0000

    Done

--- q01_pipeline/build.py
+++ q01_pipeline/build.py
@@ -1,3 +1,4 @@
+# %load q01_pipeline/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
@@ -9,5 +10,33 @@ from sklearn.metrics import roc_auc_score
 bank = pd.read_csv('data/Bank_data_to_class.csv', sep=',')
 
 # Write your solution here :
+
+le = LabelEncoder()
+
+model = RandomForestClassifier(random_state=9,class_weight = 'balanced',n_estimators=50)
+
+for column in bank.select_dtypes(include=['object']).columns.values:
+    bank[column] = le.fit_transform(bank[column])
+
+X=bank.drop(['y'],axis=1)
+y=bank['y']
+    
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size =0.3,random_state=9)
+
+def pipeline(X_train,X_test,y_train,y_test,model):
+        
+    param_grid = {'max_depth':[2,3,4,5,6,10,15,20,30],'max_features':[8,10,12,14],'max_leaf_nodes':[2,5,10,15,20]}
+    
+    gs = GridSearchCV(estimator=model,param_grid=param_grid)
+    
+    obj = gs.fit(X_train,y_train)
+    
+    y_pred = gs.predict(X_test)
+    auc = roc_auc_score(y_test,y_pred)
+    
+    return obj,auc
+
+# pipeline(X_train,X_test,y_train,y_test,model)
+
 
 
